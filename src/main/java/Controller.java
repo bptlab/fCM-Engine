fd732ae1b474f995a35d35cb3527cfc72716aec9
Stephan Haarmann <stephan.haarmann@hpi.uni-potsdam.de>
2020-12-01 08:41:22 +0100

    Added license popup to UI

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,11 +2,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.cpntools.accesscpn.engine.DaemonSimulator;
 import org.cpntools.accesscpn.engine.OSValidator;
 import org.cpntools.accesscpn.engine.Simulator;
@@ -225,6 +230,15 @@ public class Controller {
 
     @FXML protected void handleCloseAction(ActionEvent event) {
         System.exit(0);
+    }
+
+    @FXML protected void handleAboutClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/about_popup.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
     }
 
     @FXML protected void handleCompleteAction(ActionEvent event) {
